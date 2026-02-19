4ad15da59ec7af03977150582869326755fb685b
Rohit gorain <gorai123n@gmail.com>
2026-02-19 19:38:10 +0530

    Feature: Sign IN

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,7 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import auth from "../fireBase/authSdk";
 import { toast } from "react-toastify";
 import { NEW_USER_CONFIG, ALREADY_USER_CONFIG } from "../Utils/toastConfigs";
@@ -7,9 +10,19 @@ import { userProfile } from "../redux/features/userSlice";
 
 // const dispatch = useDispatch();
 
-const UserAuthentication = (isSignUPForm, email, password, dispatch) => {
+const UserAuthentication = (isSignUPForm, email, password) => {
   if (!isSignUPForm) {
     //Sign-In Logic
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode + errorMessage);
+      });
   } else {
     //Sign-UP Logic
     createUserWithEmailAndPassword(auth, email, password)
@@ -17,7 +30,6 @@ const UserAuthentication = (isSignUPForm, email, password, dispatch) => {
         const user = userCredential.user;
         toast.success("Account created successfully", NEW_USER_CONFIG);
         console.log(user);
-        dispatch(userProfile(user.uid));
       })
       .catch((error) => {
         const errorCode = error.code;
